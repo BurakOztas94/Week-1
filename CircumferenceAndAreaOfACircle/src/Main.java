bc520a48a57b860ab6253f4fc1e53c260be97c7a
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-09-15 00:18:52 +0300

    Circumference And Area Of A Circle

--- CircumferenceAndAreaOfACircle/src/Main.java
+++ CircumferenceAndAreaOfACircle/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        System.out.print ("Please enter perimeter of circle : ");
+        System.out.print (" Please enter perimeter of circle : ");
         double r = input.nextDouble ();
         double circumference = 2*r*Math.PI;
         double area=Math.PI*Math.pow ( r,2 );
-        System.out.println ("Circumference of Circle is : "+circumference + "\n"+"Area of Circle is :"+ area);
+        System.out.println (" Circumference of Circle is : "+circumference + "\n"+"Area of Circle is :"+ area);
 
     }
 }
