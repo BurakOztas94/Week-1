ca0a9e0b74123098ccafe6933c3fc31adbcaa711
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-09-19 23:05:59 +0300

    Power of Numbers

--- PowerOfNumber/src/Main.java
+++ PowerOfNumber/src/Main.java
@@ -5,22 +5,33 @@ public class Main {
         {
             //This project is not well working project
             //Because of time limit completed quickly and not added bounds
-            int n,m;
+            int n, m;
             Scanner scan = new Scanner ( System.in );
-            System.out.print ("Enter first number : ");
-            n=scan.nextInt ();
-            System.out.print ("Enter first number : ");
-            m=scan.nextInt ();
-            int total =1;
+            System.out.print ( "Enter first number : " );
+            n = scan.nextInt ();
+            System.out.print ( "Enter first number : " );
+            m = scan.nextInt ();
+            int total = 1;
 
-            for(int i=0; i<=n;i++){
-            total*=n;
+            if ( n < m )
+                {
+                    for ( int i = 0;i <= n;i++ )
+                        {
+                            total *= n;
+                        }
+                    System.out.println ( "The answer to " + n + " to the power of " + m + " is : " + total );
+                }
+            else
+                {
+                    for ( int j = 1;j <= m;j++ )
+                        {
+                            total *= n;
+                        }
+                    System.out.println ( "The answer to " + n + " to the power of " + m + " is : " + total );
 
-            }
-            System.out.println ("The answer to "+n+" to the power of "+m+" is : "+total);
 
 
 
 
-        }
-}
+                }
+        }}
