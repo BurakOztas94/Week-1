69a6a5f267e2b5de9e6bb2360d98163547beea38
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-09-20 00:41:29 +0300

    Inverted Triangle

---
+++ AtmMachineProject/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main ( String[] args )
+        {
+            String username,password;
+            Scanner scan =new Scanner (System.in);
+            int rights=3;
+
+            while (rights>0){
+                System.out.println ("Please enter your user name : ");
+                username=scan.nextLine ();
+                System.out.println ("Please enter your password name : ");
+                password=scan.nextLine ();
+
+
+            }
+
+
+
+
+        }
+}
