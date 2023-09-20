39a98383e95abd972856142859c6703a55701ebc
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-09-20 21:31:58 +0300

    Armstrong Numbers

---
+++ PrimeNumberFinder/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main ( String[] args )
+        {
+            int count=1;
+            for (int i=2;i<=100;i++){
+               if ( i%count==0 ){
+                   System.out.println (i);
+
+                }count++;
+            }
+        }
+}
