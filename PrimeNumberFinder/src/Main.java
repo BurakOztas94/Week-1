c77e0178e63018a658f65ea2b6836e51f8cf7cc5
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2023-09-21 00:29:34 +0300

    1 to 100 Prime Numbers

--- PrimeNumberFinder/src/Main.java
+++ PrimeNumberFinder/src/Main.java
@@ -1,13 +1,28 @@
 public class Main {
-
-    public static void main ( String[] args )
-        {
-            int count=1;
-            for (int i=2;i<=100;i++){
-               if ( i%count==0 ){
-                   System.out.println (i);
-
-                }count++;
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
             }
         }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
